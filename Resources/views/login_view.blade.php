e3c3b64ca776005ec74f0bab0265cd268ef01fcb
Vladimir Vlach <vladaman@gmail.com>
2022-01-05 19:06:12 +0100

    Fix for SAML URL login

--- Resources/views/login_view.blade.php
+++ Resources/views/login_view.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-10 text-right">
-            <a href="/login?sso=1" class="btn btn-primary"><em class="glyphicon glyphicon-log-in" style="margin-right: 6px"></em>
+            <a href="/saml_login?sso=1" class="btn btn-primary"><em class="glyphicon glyphicon-log-in" style="margin-right: 6px"></em>
              {{ __('Sign In with SSO') }}
           </a>
 </div>
